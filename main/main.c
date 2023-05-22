0630bd660d0dd4adf56bc495fb223992d73ba670
areejk101 <133568958+areejk101@users.noreply.github.com>
2023-05-22 13:58:14 +0500

    Update main.c

--- main/main.c
+++ main/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "GPS_parse.h"
-
+// unit test code
 int main() {
 	const char *packet ="$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,,0000*5E";
 	GPSData gpsData;
